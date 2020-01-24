24349e696f17ef9b6ead130afc6253530ed49b0e
Showmen Talukder <38407270+SoulSen@users.noreply.github.com>
2020-01-23 18:17:59 -0800

    should fix

--- CTSearch.py
+++ CTSearch.py
@@ -38,9 +38,8 @@ class CTSearch(commands.Bot):
         await self.change_presence(activity=discord.Activity(name="Reading JavaDocs | //help",
                                                              type=discord.ActivityType.playing))
         await self._prepare_cache()
-        await self.wait_until_ready()
+        self.MODULE_CHANNEL = await self.get_channel(366740283943157760)
         
-        self.MODULE_CHANNEL = await self.fetch_channel(366740283943157760)
         self.load_extension('cogs.ChatTriggers')
 
         print('READY')
